c81cb710308ab473f63baa7878592193e0386622
Denis Anikin <ad@xfenix.ru>
2014-12-05 03:27:51 +0300

    experiments with c

--- hmin_c/base.c
+++ hmin_c/base.c
@@ -3,12 +3,36 @@
 #include "dict.h"
 
 
-int main(int argc, char **argv)
-{
-    hashtable_t *hashtable = ht_create( DICT_MAX_SIZE );
-    ht_set( hashtable, "key1", "inky" );
-    printf( "%s\n", ht_get( hashtable, "key1" ) );
-    return 0;
+int main(int argc, char **argv) {
+    // hashtable_t *hashtable = ht_create( DICT_MAX_SIZE );
+    // ht_set( hashtable, "key1", "inky" );
+    // printf( "%s\n", ht_get( hashtable, "key1" ) );
+    // return 0;
+
+    int i, res, len;
+    char result[BUFSIZ];
+    regex_t preg;
+    regmatch_t pmatch[10];
+    char* src = argv[1];
+    const char* pattern = "<script>(.*?)</script>";
+
+    regcomp(&preg, pattern, REG_EXTENDED);
+
+    res = regexec(&preg, src, 10, pmatch, REG_NOTBOL);
+
+    if(res == REG_NOMATCH) {
+        printf("NO match\n");
+        exit(0);
+    }
+
+    for (i = 0; pmatch[i].rm_so != -1; i++) {
+        len = pmatch[i].rm_eo - pmatch[i].rm_so;
+        memcpy(result, src + pmatch[i].rm_so, len);
+        result[len] = 0;
+        printf("num %d: '%s'\n", i, result);
+    }
+
+    regfree(&preg);
     /*
    int i = 0;
    int res;
