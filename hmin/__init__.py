7e7256fbe786f4ee5425446288373aeb4a562353
Denis Anikin <ad@xfenix.ru>
2020-08-03 02:53:33 +0300

    Version bump, description updated

--- hmin/__init__.py
+++ hmin/__init__.py
@@ -1,6 +1,6 @@
-"""Entrypoint and version.
+"""Html minification middleware with (but not only) django support and good speed.
 """
 from .base import html_minify
 
 
-__version__: str = "0.5.1"
+__version__: str = "0.5.2"
